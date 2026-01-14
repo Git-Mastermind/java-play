12fac2cf0dd97bdb968490798f33d5b87b51cd54
Eshan Jha <eshan-jha@outlook.com>
2026-01-13 18:03:30 -0800

    modify Playground

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -2,8 +2,9 @@ package main.java;
 
 public class Playground {
     public static void main(String[] args) {
-        int num = -5;
-        int new_num = -num;
-        System.out.println(new_num);
+        int x = 5;
+        double y = 5;
+        int new_y = (int) y;
+        System.out.println(x+new_y);
     }
 }
