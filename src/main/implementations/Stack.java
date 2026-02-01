9645ac8c5bd95160c1f225b618d948e09a3dec08
Eshan Jha <eshan-jha@outlook.com>
2026-01-31 19:45:12 -0800

    make folder implementations and make a successfull implementation of Stacks

---
+++ src/main/implementations/Stack.java
@@ -0,0 +1,40 @@
+package main.implementations;
+import java.util.*;
+public class Stack<T>  {
+    ArrayList<T> stack = new ArrayList<>();
+
+    public void push(T input) {
+        stack.add(input);
+    }
+
+    public T pop() {
+        T first_element = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        return first_element;
+    }
+
+    public T peek() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public int search(T item) {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        int indexOfItem = stack.size() - stack.indexOf(item);
+        return indexOfItem;
+    }
+}
--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -4,6 +4,6 @@ import java.util.*;
 
 public class Playground {
     public static void main(String[] args) {
-
+        
     }
 }
