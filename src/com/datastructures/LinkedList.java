4cb152df19d22485cabd9e110d69816c63ce4ba2
Eshan Jha <eshan-jha@outlook.com>
2026-02-02 16:03:55 -0800

    implement print_list func in LinkedList

--- src/com/datastructures/LinkedList.java
+++ src/com/datastructures/LinkedList.java
@@ -1,13 +1,31 @@
 package com.datastructures;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LinkedList<T> {
-    T head;
-
-    public LinkedList(T head) {
-        this.head = head;
-    }
+    Node<T> head = null;
+    List<T> nodes = new ArrayList<T>();
 
     public void append(T data) {
         Node<T> new_node = new Node<>(data);
+        if (this.head == null) {
+            this.head = new_node;
+        }
+        Node<T> last_node = this.head;
+        while (last_node != null) {
+            last_node = last_node.next;
+        }
+        last_node = new_node;
+    }
+
+    public List<T> print_list() {
+        Node<T> last_node = this.head;
+        
+        while (last_node.next != null) {
+            nodes.add(last_node.data);
+            last_node = last_node.next;
+        }
+        return nodes;
     }
 }
--- src/com/datastructures/TestRunner.java
+++ src/com/datastructures/TestRunner.java
@@ -3,7 +3,7 @@ package com.datastructures;
 
 public class TestRunner {
     public static void main(String[] args) {
-
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
         
 
     }
