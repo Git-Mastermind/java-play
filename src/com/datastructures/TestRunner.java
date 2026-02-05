b91321abd2a972ccf714834a4066a97e0fea3fb9
Eshan Jha <eshan-jha@outlook.com>
2026-02-04 20:40:56 -0800

    prepare TestRunner for LinkedList implementation test

--- src/com/datastructures/TestRunner.java
+++ src/com/datastructures/TestRunner.java
@@ -3,10 +3,13 @@ package com.datastructures;
 
 public class TestRunner {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.append(1);
-        linkedList.append(2);
-        linkedList.append(3);
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.append("A");
+        linkedList.append("B");
+        linkedList.append("C");
+        linkedList.append("D");
+        System.out.println(linkedList.print_list());
+        linkedList.delete_node("B");
         System.out.println(linkedList.print_list());
 
         
