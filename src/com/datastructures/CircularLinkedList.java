966790a82759eb269ebee70a4eba28e124c0d97d
Eshan Jha <eshan-jha@outlook.com>
2026-02-12 17:39:44 -0800

    add append and printList methods to CircularLinkedList

--- src/com/datastructures/CircularLinkedList.java
+++ src/com/datastructures/CircularLinkedList.java
@@ -2,10 +2,32 @@ package com.datastructures;
 
 public class CircularLinkedList<T> {
     Node<T> head = null;
+
+    public void printList() {
+        Node<T> current = this.head;
+        String formatted = "";
+
+        while (current.next != this.head) {
+            formatted = formatted.concat(current.data + " ---> ");
+            current = current.next;
+        }
+        formatted = formatted.concat(current.data.toString() + " ---> back to head");
+        System.out.println(formatted);
+    }
     
     public void append(T data) {
+        Node<T> newNode = new Node<>(data);
+        Node<T> current = this.head;
         if (this.head == null) {
-            Node<T> newNode
+            this.head = newNode;
+            this.head.next = this.head;
+        }
+        else {
+            while (current.next != this.head) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = this.head;
         }
     }
 
--- src/com/datastructures/TestRunner.java
+++ src/com/datastructures/TestRunner.java
@@ -3,11 +3,13 @@ package com.datastructures;
 
 public class TestRunner {
     public static void main(String[] args) {
+        CircularLinkedList<String> linkedList = new CircularLinkedList<>();
         
+        linkedList.append("A");
+        linkedList.append("B");
+        linkedList.append("C");
+        linkedList.append("D");
 
-
-
-
-
+        linkedList.printList();
     }
 }
