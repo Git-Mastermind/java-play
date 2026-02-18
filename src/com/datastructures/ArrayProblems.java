31375a68322b8c9543bfe6f59d22634a337676b8
Eshan Jha <eshan-jha@outlook.com>
2026-02-17 16:42:12 -0800

    implement arrayAdvanceGame

--- src/com/datastructures/ArrayProblems.java
+++ src/com/datastructures/ArrayProblems.java
@@ -1,5 +1,25 @@
 package com.datastructures;
+import java.util.List;
+import java.lang.Math;
 
 public class ArrayProblems {
     
+    public void arrayAdvanceGame(List<Integer> nums) {
+        int furthest_reached = 0;
+        int last_index = nums.size() - 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > furthest_reached) {
+                break;
+            }
+            furthest_reached = Math.max(furthest_reached, nums.get(i) + i);
+        }
+        
+        if (furthest_reached >= last_index) {
+            System.out.println("Win!");
+        }
+        else {
+            System.out.println("Unwinnible!");
+        }
+    }
 }
--- src/com/datastructures/TestRunner.java
+++ src/com/datastructures/TestRunner.java
@@ -1,9 +1,15 @@
 package com.datastructures;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class TestRunner {
     public static void main(String[] args) {
-        DoublyLinkedList<String> linkedList = new DoublyLinkedList<>();
+        ArrayProblems arrays = new ArrayProblems();
+        List<Integer> nums = new ArrayList<>(List.of(3,2,0,0,2,0,1));
+        
+
+        arrays.arrayAdvanceGame(nums);
         
         
     }
