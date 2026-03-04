5467d36f082d9211a4b2390159b4e10475edb18d
Eshan Jha <eshan-jha@outlook.com>
2026-03-03 16:16:27 -0800

    start levelOrderTransversal

--- src/com/datastructures/BinaryTrees/BinaryTree.java
+++ src/com/datastructures/BinaryTrees/BinaryTree.java
@@ -1,11 +1,22 @@
 package com.datastructures.binaryTrees;
+import java.util.List;
+import java.util.ArrayList;
+import com.datastructures.other.Queue;
+
+
 
 public class BinaryTree<T> {
     Node<T> root;
-
+    
     public BinaryTree() {
         this.root = null;
     }
+
+    public void levelOrderTraversal() {
+        
+    }
+
+    
 
     
 }
