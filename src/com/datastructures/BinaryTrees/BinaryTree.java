e9664434199f2c5911d4ef867d3d7d03f9bc581e
Eshan Jha <eshan-jha@outlook.com>
2026-03-11 17:38:55 -0700

    remove old code in BinaryTrees and restart

--- src/com/datastructures/BinaryTrees/BinaryTree.java
+++ src/com/datastructures/BinaryTrees/BinaryTree.java
@@ -12,32 +12,15 @@ public class BinaryTree<T> {
         this.root = null;
     }
 
-    public void levelOrderTraversal(T start) {
-        Node<T> startNode = new Node<>(start);
-        levelOrderTraversalHelper(startNode);
+    public void preOrder(T data) {
+
     }
 
-    public String levelOrderTraversalHelper(Node<T> start) {
-        Queue<T> queue = new Queue<>();
-        String traversal = "";
-        queue.enqueue(start.value);
-
-        while (queue.size() > 0) {
-            traversal = traversal.concat(queue.dequeue().toString());
-
-            if (start.left != null) {
-                queue.enqueue(start.left.value);
-            }
-            if (start.right != null) {
-                queue.enqueue(start.right.value);
-            }
-            traversal = traversal.concat(queue.dequeue().toString());
-        }
-        return traversal;
-    }
+    public void preOrderHelper(Nod)
     
 
     
 
     
+}
 }
