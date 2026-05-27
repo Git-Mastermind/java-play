3770dc87c7fa8c43e03a15d019bbfaceb4389f98
Git-Mastermind <eshan-jha@outlook.com>
2026-05-27 14:24:50 -0700

    try to fix bug in ArrayListImplementation in delete method

--- src/com/datastructures/ArrayListImplementation/ArrayListImplementation.java
+++ src/com/datastructures/ArrayListImplementation/ArrayListImplementation.java
@@ -1,5 +1,26 @@
 package com.datastructures.ArrayListImplementation;
 
 public class ArrayListImplementation {
+
+    String[] array = {"a", "b", "c", "d", "e"};
+
+    public void delete(String valueToErase) {
+        String[] newArray = new String[array.length - 1];
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] != valueToErase) {
+                newArray[i] = array[i];
+            }
+            
+        }
+        turnArrayToStringRepresentation(newArray);
+    }
+
+    public void turnArrayToStringRepresentation(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
     
 }
