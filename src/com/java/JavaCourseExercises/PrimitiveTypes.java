50a0e97c39141d1cd9ebfb2d2a66250156ffef58
Git-Mastermind <eshan-jha@outlook.com>
2026-07-22 16:21:00 -0700

    add all primitive types to PrimitiveTypes.java for reference

---
+++ src/com/java/JavaCourseExercises/PrimitiveTypes.java
@@ -0,0 +1,18 @@
+package com.java.JavaCourseExercises;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        int i = 1_925_347;
+        short s = 558;
+        long l = 90000000000L;
+        byte b = 65;
+
+        float f = 6.8F;
+        double d = 9.7;
+
+        char c = 'e';
+
+        boolean bool = true;
+
+    }
+}
