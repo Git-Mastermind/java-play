900c2bef9aa8cf8a6d3d6420f1133969e3d9072b
Git-Mastermind <eshan-jha@outlook.com>
2026-07-22 13:41:53 -0700

    practice StringBuilder

--- src/com/java/JavaCourseExercises/Welcome.java
+++ src/com/java/JavaCourseExercises/Welcome.java
@@ -2,17 +2,11 @@ package com.java.JavaCourseExercises;
 
 public class Welcome {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        // basic print
-        System.out.println("Hello");
-        // print with line change after
-        System.out.print("Hello \n Hellllllllp \n");
-        /* basic print with line change using \n
-        \n adds a line change  */
-        System.out.println("Hello");
-        // print ln with line change, so double line change
-        System.out.println("\n Yooooo");
-        // comments explain the why not the what, so the comments above are bad comments
+        StringBuilder helloName = new StringBuilder("Hello Eshan");
+        StringBuilder helloJava = helloName;
+        System.out.println(helloName);
+        helloName.append(" and Hello Java");
+        System.out.println(helloJava);
         
     }
 }
