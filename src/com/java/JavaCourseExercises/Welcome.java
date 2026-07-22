7163216811671b08b8ee21ae6ad42058a6cebb5e
Git-Mastermind <eshan-jha@outlook.com>
2026-07-21 17:43:09 -0700

    modify Welcome.java

--- src/com/java/JavaCourseExercises/Welcome.java
+++ src/com/java/JavaCourseExercises/Welcome.java
@@ -1,5 +1,10 @@
 package com.java.JavaCourseExercises;
 
 public class Welcome {
-    
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.println("Hello");
+        System.out.print("Hello \n Hellllllllp \n");
+        System.out.println("Hello");
+    }
 }
