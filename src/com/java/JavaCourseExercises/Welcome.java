b0415096cfcaec21671a5ab1b660c41dd019b859
Git-Mastermind <eshan-jha@outlook.com>
2026-07-22 16:17:15 -0700

    modify Welcome.java

--- src/com/java/JavaCourseExercises/Welcome.java
+++ src/com/java/JavaCourseExercises/Welcome.java
@@ -2,8 +2,7 @@ package com.java.JavaCourseExercises;
 
 public class Welcome {
     public static void main(String[] args) {
-        long longNumber = 2_147_483_648L;
-        System.out.println(longNumber);
+        char character = 'e';
         
     }
 }
