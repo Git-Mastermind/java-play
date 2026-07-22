9276a09041b97ead370806f2897ecd52d0b11221
Git-Mastermind <eshan-jha@outlook.com>
2026-07-21 17:51:24 -0700

    practice printing statements

--- src/com/java/JavaCourseExercises/Welcome.java
+++ src/com/java/JavaCourseExercises/Welcome.java
@@ -3,8 +3,14 @@ package com.java.JavaCourseExercises;
 public class Welcome {
     public static void main(String[] args) {
         System.out.print("Hello");
+        // basic print
         System.out.println("Hello");
+        // print with line change after
         System.out.print("Hello \n Hellllllllp \n");
+        /* basic print with line change using \n
+        \n adds a line change  */
         System.out.println("Hello");
+        // print ln with line change, so double line change
+        System.out.println("\n Yooooo");
     }
 }
