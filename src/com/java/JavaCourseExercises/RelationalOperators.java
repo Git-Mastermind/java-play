12d74e767a5bf44a892a9c707213d3b0564568f8
Git-Mastermind <eshan-jha@outlook.com>
2026-07-23 15:47:08 -0700

    make RelationalOperators and add comparisions

---
+++ src/com/java/JavaCourseExercises/RelationalOperators.java
@@ -0,0 +1,19 @@
+package com.java.JavaCourseExercises;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 9;
+
+        int a = 10;
+        int b = 900;
+
+        boolean comparision = x < y || a < b;
+
+        System.out.println(comparision);
+
+        
+        
+        
+    }
+}
