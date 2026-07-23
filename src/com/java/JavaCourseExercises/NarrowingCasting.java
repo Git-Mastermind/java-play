e9c5a7cb974aac06727471635e43f6ec2fd1b454
Git-Mastermind <eshan-jha@outlook.com>
2026-07-23 14:17:20 -0700

    make NarrowingCasting.java

---
+++ src/com/java/JavaCourseExercises/NarrowingCasting.java
@@ -0,0 +1,23 @@
+package com.java.JavaCourseExercises;
+
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        float floatNumber = 6.8F; // remember to add F because java defaults decimal numbers to double
+        System.out.println(floatNumber); // will print 6.8
+        
+        int intNum = (int) floatNumber; // casting int data type to floatNumber (6.8) and assigning that to integer num
+        System.out.println(intNum); // when casting int to float or double the number of rounded down so output would be 6
+
+        byte byteNumber = 32;
+        System.out.println(byteNumber); // output: 32
+
+        int intNum2 = (int) byteNumber; // casting type int onto byteNumber (32)
+        System.out.println(intNum2); // prediction: output will still be the same::: output was the same
+
+        int intNum3 = 350;
+        System.out.println(intNum3); // output: 350
+
+        byte byteNum2 = (byte) intNum3; // casting byte onto intNum3 (350)
+        System.out.println(byteNum2); 
+    }
+}
