bf27a9e78033e68a6932a06313b81df30deae64d
Git-Mastermind <eshan-jha@outlook.com>
2026-07-23 16:09:43 -0700

    modify conditionals, add exercise from course

---
+++ src/com/java/JavaCourseExercises/Conditionals.java
@@ -0,0 +1,21 @@
+package com.java.JavaCourseExercises;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 9;
+
+        if (x > y) {
+            System.out.println(x);
+        }
+        else if (y > x) {
+            System.out.println(y);
+        }
+        else {
+            System.out.println("xy");
+        }
+
+        
+        
+    }
+}
