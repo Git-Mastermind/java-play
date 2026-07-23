ed0fd1e3caade3d22adf2ae436bb762193ac1776
Git-Mastermind <eshan-jha@outlook.com>
2026-07-23 12:21:20 -0700

    make CodeTests for running programs and Casting to learn casting

---
+++ src/com/java/JavaCourseExercises/Casting.java
@@ -0,0 +1,13 @@
+package com.java.JavaCourseExercises;
+
+public class Casting {
+    public static void main(String[] args) {
+        byte b = 90;
+        int a = 900;
+
+        a = b; // is valid because b is a smaller value than a and can fit in the integer range
+        b = a; // is not valid because a is larger than b and you can't fit a large range into a smaller range
+        b = (byte) a; // is valid because a is being converted into 8 bits of memory by a process called truncation
+        
+    }
+}
---
+++ src/com/java/JavaCourseExercises/CodeTests.java
@@ -0,0 +1,7 @@
+package com.java.JavaCourseExercises;
+
+public class CodeTests {
+    public static void main(String[] args) {
+        
+    }
+}
