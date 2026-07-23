19a5a101b2e29731355ec9846fb0382a6ef19294
Git-Mastermind <eshan-jha@outlook.com>
2026-07-23 16:25:35 -0700

    make Ternaryoperator and practice TernaryOperator

---
+++ src/com/java/JavaCourseExercises/TernaryOperator.java
@@ -0,0 +1,11 @@
+package com.java.JavaCourseExercises;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int num = 990200224;
+
+        boolean result = num%2==0?true:false;
+
+        System.out.println(result);
+    }
+}
