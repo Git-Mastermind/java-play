41cbec7df574b599f4c8e3c3fec088d6caf77672
Git-Mastermind <eshan-jha@outlook.com>
2026-07-24 11:23:17 -0700

    practice For Loops

--- src/com/java/JavaCourseExercises/ForLoop.java
+++ src/com/java/JavaCourseExercises/ForLoop.java
@@ -1,5 +1,9 @@
 package com.java.JavaCourseExercises;
 
 public class ForLoop {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(i);
+        }
+    }
 }
