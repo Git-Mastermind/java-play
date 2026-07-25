9a851900e6e6a4c6c0370d72aa08315646d9b0c2
Git-Mastermind <eshan-jha@outlook.com>
2026-07-25 14:21:00 -0700

    practice nested while loops

--- src/com/java/JavaCourseExercises/WhileLoop.java
+++ src/com/java/JavaCourseExercises/WhileLoop.java
@@ -2,10 +2,15 @@ package com.java.JavaCourseExercises;
 
 public class WhileLoop {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
 
-        while (i <= 5) {
-            System.out.println("Hi");
+        while (i <= 4) {
+            System.out.println("Hi" + i);
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("Hello" + j);
+                j++;
+            }
             i++;
         }
     }
