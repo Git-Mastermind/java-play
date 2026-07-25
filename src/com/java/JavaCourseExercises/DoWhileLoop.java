6e404de54ebbf23d861673e8a76a7538f2431ddd
Git-Mastermind <eshan-jha@outlook.com>
2026-07-25 14:29:47 -0700

    make DoWhile and practice

---
+++ src/com/java/JavaCourseExercises/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.java.JavaCourseExercises;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 5;
+        do
+        {
+            System.out.println("Hi");
+            i++;
+        }
+        while (i <= 4);
+    }
+}
