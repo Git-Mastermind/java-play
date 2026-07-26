11ec4f9688b66ef8e9d731bb8b4d368608762900
Git-Mastermind <eshan-jha@outlook.com>
2026-07-25 18:11:36 -0700

    make another for loop file to practice more

---
+++ src/com/java/JavaCourseExercises/ForLoopPractice.java
@@ -0,0 +1,15 @@
+package com.java.JavaCourseExercises;
+
+public class ForLoopPractice {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 7; i++) 
+        {
+            System.out.println("DAY " + i);
+            for (int j = 1; j <= 24; j++)
+            {
+                System.out.println("HOUR " + j);
+            }
+        }
+    }
+}
