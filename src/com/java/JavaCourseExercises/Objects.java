02ec351da5d4178cbe67a821aadc1baa1fb35190
Git-Mastermind <eshan-jha@outlook.com>
2026-07-27 12:34:26 -0700

    make TernaryOperatorPratice to practice more ternary operator

---
+++ src/com/java/JavaCourseExercises/Objects.java
@@ -0,0 +1,9 @@
+package com.java.JavaCourseExercises;
+
+public class Objects {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.add(5,6));
+    }
+}
