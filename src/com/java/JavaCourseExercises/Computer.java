a98e6d991df0d004036dc37353ff949aaf583b70
Git-Mastermind <eshan-jha@outlook.com>
2026-07-27 13:30:31 -0700

    make Computer class with getMeAPen method and tet

---
+++ src/com/java/JavaCourseExercises/Computer.java
@@ -0,0 +1,7 @@
+package com.java.JavaCourseExercises;
+
+public class Computer {
+    public String getMeAPen(int money) {
+        return money >= 10?"Pen":"more money (constrains: money >= 10)";
+    }
+}
---
+++ src/com/java/JavaCourseExercises/Demo.java
@@ -0,0 +1,11 @@
+package com.java.JavaCourseExercises;
+
+public class Demo {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+
+        String pen = computer.getMeAPen(11);
+        System.out.println(pen);
+    }
+
+}
