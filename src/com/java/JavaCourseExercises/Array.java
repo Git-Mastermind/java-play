3b65e7bf04186694b93135b35f8874c055c99782
Git-Mastermind <eshan-jha@outlook.com>
2026-07-29 14:41:19 -0700

    make Array

---
+++ src/com/java/JavaCourseExercises/Array.java
@@ -0,0 +1,13 @@
+package com.java.JavaCourseExercises;
+
+public class Array {
+    public static void main(String[] args) {
+        int num[] = new int[4];
+        num[0] = 0;
+        num[1] = 1;
+        num[2] = 2;
+        num[3] = 3;
+
+        
+    }
+}
