645bc28d8290e8dc9b962d63617591bc85f7da71
Git-Mastermind <eshan-jha@outlook.com>
2026-07-29 14:43:06 -0700

    loop through each value in nums and print it

--- src/com/java/JavaCourseExercises/Array.java
+++ src/com/java/JavaCourseExercises/Array.java
@@ -2,11 +2,15 @@ package com.java.JavaCourseExercises;
 
 public class Array {
     public static void main(String[] args) {
-        int num[] = new int[4];
-        num[0] = 0;
-        num[1] = 1;
-        num[2] = 2;
-        num[3] = 3;
+        int nums[] = new int[4];
+        nums[0] = 0;
+        nums[1] = 1;
+        nums[2] = 2;
+        nums[3] = 3;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(nums[i]);
+        }
 
         
     }
