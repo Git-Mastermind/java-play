c807642a034b86f1c8f8ddb338a8c90ffb9f7750
Git-Mastermind <eshan-jha@outlook.com>
2026-08-01 09:20:09 -0700

    make ProductExceptItself O(N)

---
+++ src/com/java/ProductExceptItselfEfficient.java
@@ -0,0 +1,35 @@
+package com.java;
+import java.util.Arrays;
+
+public class ProductExceptItselfEfficient {
+    public void productExceptItselfEfficient(int[] inputArray) {
+        int product = 1;
+        int zeroCount = 0;
+        int[] resultArray = new int[inputArray.length];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == 0) {
+                zeroCount++;
+            }
+            else {
+                product *= inputArray[i];
+            }
+        }
+        if (zeroCount != 0) {
+            for (int i = 0; i < inputArray.length; i++) {
+                if (inputArray[i] == 0) {
+                    resultArray[i] = product;
+                }
+                else {
+                    resultArray[i] = 0;
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < inputArray.length; i++) {
+                resultArray[i] = product / inputArray[i];
+            }
+        }
+        System.out.println(Arrays.toString(resultArray));
+    }
+}
---
+++ src/com/java/ProductExceptItselfEfficientTester.java
@@ -0,0 +1,9 @@
+package com.java;
+
+public class ProductExceptItselfEfficientTester {
+    public static void main(String[] args) {
+        ProductExceptItselfEfficient tester = new ProductExceptItselfEfficient();
+        int[] inputArray = {3,1,1,0,6};
+        tester.productExceptItselfEfficient(inputArray);
+    }
+}
