fe0bd798421bcb1a36ed46035ac8799be53c6740
Git-Mastermind <eshan-jha@outlook.com>
2026-07-31 17:56:02 -0700

    modify Palindrome and Tester

--- src/com/java/GrokkingInterviewPatterns/IsPalindrome.java
+++ src/com/java/GrokkingInterviewPatterns/IsPalindrome.java
@@ -4,10 +4,39 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IsPalindrome {
-    public void isPalindrome(String word) {
-        String[] strArray = word.split("");
-        List<String> str = new ArrayList<>(Arrays.asList(strArray));
 
-        System.out.println(str);
+    public List<String> removeSpaces(String word) {
+        String[] array = word.split("");
+        List<String> sentence = new ArrayList<>(Arrays.asList(array));
+
+        for (int i = 0; i <= sentence.size() - 1; i++) {
+            if (sentence.get(i).equals(" ")) {
+                sentence.remove(i);
+            }
+        }
+        return sentence;
+    }
+
+
+    public boolean isPalindrome(String word) {
+        List<String> sentence = this.removeSpaces(word);
+
+        String leftPointer = sentence.get(0);
+        String rightPointer = sentence.get(sentence.size() - 1);
+        int leftCurrentIndex = 0;
+        int rightCurrentIndex = sentence.size() - 1;
+
+        while (leftPointer.equals(rightPointer)) {
+            if (leftCurrentIndex == sentence.size() - 1 && rightCurrentIndex == 0) {
+                return true;
+            }
+            leftCurrentIndex++;
+            rightCurrentIndex--;
+
+            leftPointer = sentence.get(leftCurrentIndex);
+            rightPointer = sentence.get(rightCurrentIndex);
+        }
+        return false;
+
     }
 }
--- src/com/java/GrokkingInterviewPatterns/Tester.java
+++ src/com/java/GrokkingInterviewPatterns/Tester.java
@@ -1,5 +1,9 @@
 package com.java.GrokkingInterviewPatterns;
 
 public class Tester {
-    
+    public static void main(String[] args) {
+        IsPalindrome palindromeObj = new IsPalindrome();
+        boolean isPalindrome = palindromeObj.isPalindrome("wsfgf");
+        System.out.println(isPalindrome);
+    }
 }
