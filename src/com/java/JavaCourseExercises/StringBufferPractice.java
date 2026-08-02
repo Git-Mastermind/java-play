ed322f03b63c6726a354da2f211c9620834b9de5
Git-Mastermind <eshan-jha@outlook.com>
2026-08-02 14:34:15 -0700

    make StringBuffer and practice

---
+++ src/com/java/JavaCourseExercises/StringBufferPractice.java
@@ -0,0 +1,8 @@
+package com.java.JavaCourseExercises;
+
+public class StringBufferPractice {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Eshan Jha");
+        System.out.println(sb.substring(0, 2));
+    }
+}
