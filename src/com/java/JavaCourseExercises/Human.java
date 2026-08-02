10cc2002f947348570040f87cda48af0ea7764e5
Git-Mastermind <eshan-jha@outlook.com>
2026-08-02 16:43:14 -0700

    make constructor in Human.java that accepts String name and int age

--- src/com/java/JavaCourseExercises/Human.java
+++ src/com/java/JavaCourseExercises/Human.java
@@ -4,6 +4,11 @@ public class Human {
     private int age;
     private String name;
 
+    public Human(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
 
     public int age() {
         return age;
