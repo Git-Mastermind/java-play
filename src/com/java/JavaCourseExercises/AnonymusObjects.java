ff1c2081d859ddc707efb7dcf77367446000a1ba
Git-Mastermind <eshan-jha@outlook.com>
2026-08-03 15:46:23 -0700

    create AnonymusObjects

---
+++ src/com/java/JavaCourseExercises/AnonymusObjects.java
@@ -0,0 +1,8 @@
+package com.java.JavaCourseExercises;
+
+public class AnonymusObjects {
+    public AnonymusObjects() 
+    {
+        System.out.println("object created");
+    }
+}
---
+++ src/com/java/JavaCourseExercises/AnonymusObjectsPractice.java
@@ -0,0 +1,8 @@
+package com.java.JavaCourseExercises;
+
+public class AnonymusObjectsPractice {
+    public static void main(String[] args)
+    {
+        new AnonymusObjects();
+    }
+}
