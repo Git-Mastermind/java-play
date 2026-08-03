3bc6d498b0e761b26cb4bc65d1aa42f35d2ad184
Git-Mastermind <eshan-jha@outlook.com>
2026-08-03 15:56:05 -0700

    make calculator with basic operations, advanced calculator extends calculator

---
+++ src/com/java/JavaCourseExercises/Inheritance/AdvancedCalculator.java
@@ -0,0 +1,7 @@
+package com.java.JavaCourseExercises.Inheritance;
+
+public class AdvancedCalculator extends Calculator {
+    public void power(int base, int power) {
+        System.out.println("power");
+    }
+}
---
+++ src/com/java/JavaCourseExercises/Inheritance/CalcTest.java
@@ -0,0 +1,11 @@
+package com.java.JavaCourseExercises.Inheritance;
+
+public class CalcTest {
+
+    public static void main(String[] args) {
+        AdvancedCalculator calc = new AdvancedCalculator();
+
+        calc.power(3,4);
+    }
+    
+}
