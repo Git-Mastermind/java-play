aa3a598c09fe27e7b41c3193db299264449b496a
Git-Mastermind <eshan-jha@outlook.com>
2026-08-04 10:38:04 -0700

    modify B and Demo

--- src/com/java/JavaCourseExercises/Super/B.java
+++ src/com/java/JavaCourseExercises/Super/B.java
@@ -1,11 +1,13 @@
 package com.java.JavaCourseExercises.Super;
 
 public class B extends A {
-    public B() {
+    public B() {    
+        this(5);
         System.out.println("in B");
     }
 
     public B(int n) {
+        super();
         System.out.println("in B int");
     }
 }
--- src/com/java/JavaCourseExercises/Super/Demo.java
+++ src/com/java/JavaCourseExercises/Super/Demo.java
@@ -2,7 +2,7 @@ package com.java.JavaCourseExercises.Super;
 
 public class Demo {
     public static void main(String[] args) {
-        B obj = new B(5);
+        B obj = new B();
     }
 
 }
