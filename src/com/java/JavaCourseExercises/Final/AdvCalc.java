51f8ca1e0c3e55ff4aea7bab59bb50f7ce3a1812
Git-Mastermind <eshan-jha@outlook.com>
2026-08-04 18:17:22 -0700

    make Calc, AdvCalc and Demo to test final. make Calc final class and make method add final to destroy overriding

---
+++ src/com/java/JavaCourseExercises/Final/AdvCalc.java
@@ -0,0 +1,5 @@
+package com.java.JavaCourseExercises.Final;
+
+public class AdvCalc {
+    
+}
---
+++ src/com/java/JavaCourseExercises/Final/Calc.java
@@ -0,0 +1,7 @@
+package com.java.JavaCourseExercises.Final;
+
+public final class Calc {
+    public final void add(int n1, int n2) {
+        System.out.println(n1 + n2);
+    }
+}
