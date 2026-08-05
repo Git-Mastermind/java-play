d8d6d8a4cc656f0f1be75c2b0bd4a3b95863543a
Git-Mastermind <eshan-jha@outlook.com>
2026-08-05 11:37:49 -0700

    use parse int to turn String number into Integer number

--- src/com/java/JavaCourseExercises/WrapperClass.java
+++ src/com/java/JavaCourseExercises/WrapperClass.java
@@ -5,6 +5,9 @@ public class WrapperClass {
         int num = 7;
         Integer num1 = num;
         int num2 = num1.intValue();
-        System.out.println(num2);
+        
+        String str = "433487563";
+        int num3 = Integer.parseInt(str);
+        System.out.println(num3);
     }
 }
