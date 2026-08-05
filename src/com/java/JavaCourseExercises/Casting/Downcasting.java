9201a05205ac640b5b23a6c243600161c440454b
Git-Mastermind <eshan-jha@outlook.com>
2026-08-05 10:34:16 -0700

    downcast double to int and print

---
+++ src/com/java/JavaCourseExercises/Casting/Downcasting.java
@@ -0,0 +1,9 @@
+package com.java.JavaCourseExercises.Casting;
+
+public class Downcasting {
+    public static void main(String[] args) {
+        double d = 6;
+        int i = (int) d;
+        System.out.println(i);
+    }
+}
