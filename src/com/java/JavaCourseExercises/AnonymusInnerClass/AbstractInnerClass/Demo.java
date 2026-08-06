ff5ffc9cfc4ab55ebc519a84cf8fce73e29345a2
Git-Mastermind <eshan-jha@outlook.com>
2026-08-06 09:18:02 -0700

    implement abstract method show in A class using inner class

--- src/com/java/JavaCourseExercises/AnonymusInnerClass/AbstractInnerClass/Demo.java
+++ src/com/java/JavaCourseExercises/AnonymusInnerClass/AbstractInnerClass/Demo.java
@@ -3,11 +3,14 @@ package com.java.JavaCourseExercises.AnonymusInnerClass.AbstractInnerClass;
 public class Demo {
     public static void main(String[] args) {
         A obj = new A() {
-        public void show() {
-            System.out.println("in implemented show");
-        }
+            public void show() {
+                System.out.println("in implememted show");
+            }
+        };
+
+        obj.show();
     }; 
     }
     
     
-}
+
