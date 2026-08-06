7a30ebea1ae72916be55cc83e5d850a527244951
Git-Mastermind <eshan-jha@outlook.com>
2026-08-06 15:56:59 -0700

    make Customer class

--- src/com/java/BankSystem/Customer.java
+++ src/com/java/BankSystem/Customer.java
@@ -1,5 +1,11 @@
 package com.java.BankSystem;
 
 public class Customer {
-    
+    private String name;
+    private int balance;
+
+    public Customer(String name, int balance) {
+        this.name = name;
+        this.balance = balance;
+    }
 }
