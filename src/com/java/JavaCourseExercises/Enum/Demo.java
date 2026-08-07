948345f12ce3ac18e0b70b3d37533ae36d63d85f
Git-Mastermind <eshan-jha@outlook.com>
2026-08-07 14:51:01 -0700

    add switch statement in Demo for each enum result

--- src/com/java/JavaCourseExercises/Enum/Demo.java
+++ src/com/java/JavaCourseExercises/Enum/Demo.java
@@ -6,10 +6,22 @@ enum Status {
 
 public class Demo {
     public static void main(String[] args) {
-        Status[] statusOptions = Status.values();
-    
-        for (Status status : statusOptions) {
-            System.out.println(status);
+        Status status = Status.Success;
+
+        switch (status) {
+            case Running:
+                System.out.println("Please wait..");
+                break;
+            case Pending:
+                System.out.println("Pending..");
+                break;
+            case Failed:
+                System.out.println("Please try again later (failed)");
+                break;
+            case Success:
+                System.out.println("Success!!");
+                break;
         }
+
     }
 }
