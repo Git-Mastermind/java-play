ff071e3c781818e8549d69bf46128c36bd8a8d2c
Git-Mastermind <eshan-jha@outlook.com>
2026-08-06 19:00:57 -0700

    all final and statuc instance variables to Demo, int age and String nae

--- src/com/java/JavaCourseExercises/Interface/Demo.java
+++ src/com/java/JavaCourseExercises/Interface/Demo.java
@@ -1,6 +1,11 @@
 package com.java.JavaCourseExercises.Interface;
 
 interface Demo {
+    int age = 14;
+    String name = "Eshan";
+
+
+
     void show();
     void config();
 }
---
+++ src/com/java/JavaCourseExercises/Interface/Test.java
@@ -0,0 +1,8 @@
+package com.java.JavaCourseExercises.Interface;
+
+public class Test {
+    public static void main(String[] args) {
+        Demo obj = new Implementer();
+        
+    }
+}
