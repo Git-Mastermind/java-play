5e43b2d9d1e5c9195911988c61075ef56c2f658c
Git-Mastermind <eshan-jha@outlook.com>
2026-08-06 18:56:25 -0700

    implementer implements demo

---
+++ src/com/java/JavaCourseExercises/Interface/Implementer.java
@@ -0,0 +1,11 @@
+package com.java.JavaCourseExercises.Interface;
+
+public class Implementer implements Demo {
+    public void show() {
+        System.out.println("in IMplementer show");
+    }
+
+    public void config() {
+        System.out.println("in implementer config");
+    }
+}
