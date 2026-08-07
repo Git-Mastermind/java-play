b9f0ad704c4d397c670581cd444b870bfdfb582a
Git-Mastermind <eshan-jha@outlook.com>
2026-08-07 15:04:43 -0700

    make getBalance getter in Account and constructor which takes balance, make getBalance in DeutscheBank and newbankAccount

--- src/com/java/BankSystem/Account.java
+++ src/com/java/BankSystem/Account.java
@@ -1,5 +1,13 @@
 package com.java.BankSystem;
 
 public class Account {
-    
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
--- src/com/java/BankSystem/DeutscheBank.java
+++ src/com/java/BankSystem/DeutscheBank.java
@@ -5,4 +5,14 @@ public class DeutscheBank {
         Customer customer = new Customer(name);
         return customer;
     }
+
+    public Account newBankAccount(String name, int balance) {
+        Account account = new Account(balance);
+        return account;
+    }
+
+
+    public int getBalance(Account account) {
+        return account.getBalance();
+    }
 }
