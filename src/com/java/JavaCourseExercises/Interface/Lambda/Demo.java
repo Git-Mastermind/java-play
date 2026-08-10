a07689c9034930c15883b4969ada537f07126101
Git-Mastermind <eshan-jha@outlook.com>
2026-08-09 17:01:57 -0700

    use lambda in add method interface

--- src/com/java/JavaCourseExercises/Interface/Lambda/Demo.java
+++ src/com/java/JavaCourseExercises/Interface/Lambda/Demo.java
@@ -1,5 +1,16 @@
 package com.java.JavaCourseExercises.Interface.Lambda;
 
+@FunctionalInterface
+interface A {
+    int add(int num1, int num2);
+}
+
 public class Demo {
+    public static void main(String[] args) {
+        A obj = (i,j) -> i + j;
+        int result = obj.add(5,4);
+        System.out.println(result);
+    }
+
     
 }
