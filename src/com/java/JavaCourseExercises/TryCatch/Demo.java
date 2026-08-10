51b11e73af0bca751843a87deaa7f55e6b4f4f6b
Git-Mastermind <eshan-jha@outlook.com>
2026-08-10 11:32:58 -0700

    feat: add different catch blocks for ArithmeticException, IndexOUtOfBoundsException and just Exception for default

--- src/com/java/JavaCourseExercises/TryCatch/Demo.java
+++ src/com/java/JavaCourseExercises/TryCatch/Demo.java
@@ -2,16 +2,25 @@ package com.java.JavaCourseExercises.TryCatch;
 
 public class Demo {
     public static void main(String[] args) {
-        int i = 1;
-        int j = 0;
+        int[] nums = {0,1,5,6,2};
         try {
-            j = 18/i;
-            System.out.println(j);
+            System.out.println(nums[0]);
+            int num0 = nums[0];
+            int num5 = nums[5];
+            int result = num5/num0;
+            
         }
 
-        catch(Exception exception) {
-            System.out.println("Exception! Caught " + exception);
+        catch(ArithmeticException error) {
+            System.out.println("cannot divide by zero");
         }
+        catch (IndexOutOfBoundsException error) {
+            System.out.println("index out of bounds");
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong" + e);
+        }
+        
 
     }
 }
