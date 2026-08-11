db3e6ac2cef3dc04cc4aada0061b91582ab596be
Git-Mastermind <eshan-jha@outlook.com>
2026-08-11 13:33:53 -0700

    make anonymus inner class into Lambda

--- src/com/java/JavaCourseExercises/ThreadPractice.java
+++ src/com/java/JavaCourseExercises/ThreadPractice.java
@@ -44,43 +44,35 @@ package com.java.JavaCourseExercises;
 
 public class ThreadPractice {
     public static void main(String[] args) {
-        Runnable obj1 = new Runnable() {
-            public void run() {
-                for (int i = 0; i <= 5; i++) {
-                    System.out.println("hi");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        Runnable obj1 = () -> {
+            for (int i = 0; i<=5; i++) {
+                System.out.println("hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         };
 
-        Runnable obj2 = new Runnable() {
-            public void run() {
-                for (int i = 0; i <= 5; i++) {
-                    System.out.println("hello");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        Runnable obj2 = () -> {
+            for (int i = 0; i<=5; i++) {
+                System.out.println("hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         };
-
 
         Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);   
-        
+        Thread t2 = new Thread(obj2);
+
         t1.start();
-        try {
-            Thread.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         t2.start();
             
     }
 }
+        
+
