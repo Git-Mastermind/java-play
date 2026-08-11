28ea8faafd55e38eebb5af9cf6985336e41423bd
Git-Mastermind <eshan-jha@outlook.com>
2026-08-11 14:06:02 -0700

    change reference of nums from Collections to List to use getIndex

--- src/com/java/JavaCourseExercises/CollectionsPractice.java
+++ src/com/java/JavaCourseExercises/CollectionsPractice.java
@@ -1,5 +1,10 @@
 package com.java.JavaCourseExercises;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CollectionsPractice {
-    
+    public static void main(String[] args ) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(9);
+    }
 }
