cb7072e057dff99ec81534d68a84245d3f58898b
Git-Mastermind <eshan-jha@outlook.com>
2026-08-11 13:29:33 -0700

    remove class A and B, use anonymus inner class instead

--- src/com/java/JavaCourseExercises/ThreadPractice.java
+++ src/com/java/JavaCourseExercises/ThreadPractice.java
@@ -1,56 +1,85 @@
 package com.java.JavaCourseExercises;
 
-class A implements Runnable {
-    @Override
-    public void run() {
-        for (int i = 0; i <= 5; i++) {
-            System.out.println("Hi");
+// class A implements Runnable {
+//     @Override
+//     public void run() {
+//         for (int i = 0; i <= 5; i++) {
+//             System.out.println("Hi");
             
-            try {
-                Thread.sleep(1);
-            }
-            catch (InterruptedException e) {
-                System.out.println("Sleep was interrupted!");
-            }
-            catch (Exception e) {
-                System.out.println("Something went wrong");
-            }
+//             try {
+//                 Thread.sleep(1);
+//             }
+//             catch (InterruptedException e) {
+//                 System.out.println("Sleep was interrupted!");
+//             }
+//             catch (Exception e) {
+//                 System.out.println("Something went wrong");
+//             }
 
-        }
+//         }
         
-    }
-}
+//     }
+// }
 
-class B implements Runnable {
-    @Override
-    public void run() {
-        for (int i = 0; i <= 5; i++) {
-            System.out.println("Hello");
+// class B implements Runnable {
+//     @Override
+//     public void run() {
+//         for (int i = 0; i <= 5; i++) {
+//             System.out.println("Hello");
 
-        try {
-            Thread.sleep(1);
-        }
-        catch (InterruptedException e) {
-            System.out.println("Sleep was interrupted!");
-        }
-        catch (Exception e) {
-            System.out.println("Something went wrong");
-        }
+//         try {
+//             Thread.sleep(1);
+//         }
+//         catch (InterruptedException e) {
+//             System.out.println("Sleep was interrupted!");
+//         }
+//         catch (Exception e) {
+//             System.out.println("Something went wrong");
+//         }
             
-        }
+//         }
         
-    }
-}
+//     }
+// }
 
 public class ThreadPractice {
     public static void main(String[] args) {
-        Runnable a = new A();
-        Runnable b = new B();
+        Runnable obj1 = new Runnable() {
+            public void run() {
+                for (int i = 0; i <= 5; i++) {
+                    System.out.println("hi");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
 
-        Thread t1 = new Thread(a);
-        Thread t2 = new Thread(b);   
+        Runnable obj2 = new Runnable() {
+            public void run() {
+                for (int i = 0; i <= 5; i++) {
+                    System.out.println("hello");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);   
         
         t1.start();
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         t2.start();
             
     }
