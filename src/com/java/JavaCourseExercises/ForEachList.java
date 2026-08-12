de2fa1c1d84b5d9b5c924b3f247d86957bbd0f21
Git-Mastermind <eshan-jha@outlook.com>
2026-08-12 12:03:27 -0700

    use consumer to write multi line code to do operations on each number

---
+++ src/com/java/JavaCourseExercises/ForEachList.java
@@ -0,0 +1,22 @@
+package com.java.JavaCourseExercises;
+import java.util.List;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class ForEachList {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(4,5,7,3,2,6);
+
+        Consumer<Integer> consumer = (Integer n) -> {
+            if (n%2==0) {
+                System.out.println(n + " --- Even");
+            }
+            else {
+                System.out.println(n + " --- Odd");
+            }
+            
+        };
+
+        nums.forEach(consumer);
+    }
+}
