19c4c0d2e4384da9e7de5fd52a4a0a1d79ce22fe
Git-Mastermind <eshan-jha@outlook.com>
2026-08-19 16:46:28 -0700

    define prepend presistant bug

--- src/com/datastructures/linkedList/circular/CircularLinkedList.java
+++ src/com/datastructures/linkedList/circular/CircularLinkedList.java
@@ -5,6 +5,7 @@ public class CircularLinkedList<T> {
 
     public CircularLinkedList(Node<T> head) {
         this.head = head;
+        this.head.next = this.head;
     }
 
     public int size() {
@@ -38,19 +39,32 @@ public class CircularLinkedList<T> {
             current = current.next;
         }
         current.next = nodeToAppend;
-        
+
     }
 
     public void formattedPrint() {
         Node<T> current = this.head;
         StringBuilder formattedLlist = new StringBuilder();
 
-        while (current.next.next != this.head) {
+        while (current.next != this.head) {
             formattedLlist.append(current.data + " ---> ");
             current = current.next;
         }
         formattedLlist.append(current.data + " ---> head");
         System.out.println(formattedLlist);
+    }
+
+    public void prepend(T value) {
+        Node<T> nodeToPrepend = new Node<>(value, this.head);
+
+        if (this.size() == 0) {
+            this.head = nodeToPrepend;
+        }
+
+        this.head = nodeToPrepend;
+
+        
+
     }
 
 }
