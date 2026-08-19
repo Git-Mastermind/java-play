0028b1695a44f8cf5b9877af251e7707496e3988
Git-Mastermind <eshan-jha@outlook.com>
2026-08-18 19:51:35 -0700

    make append, size and formattedPrint in circular Linked List and add access modifiers to instance variables

--- src/com/datastructures/linkedList/circular/CircularLinkedList.java
+++ src/com/datastructures/linkedList/circular/CircularLinkedList.java
@@ -1,11 +1,47 @@
 package com.datastructures.linkedlist.circular;
 
 public class CircularLinkedList<T> {
-    Node<T> head;
+    private Node<T> head;
 
     public CircularLinkedList(Node<T> head) {
         this.head = head;
     }
 
-    
+    public int size() {
+        Node<T> current = this.head;
+        int counter = 1;
+
+        while (current.next != this.head) {
+            counter++;
+            current = current.next;
+        }
+        return counter;
+    }
+
+    public void append(T value) {
+        Node<T> newNode = new Node<>(value, null);
+
+        if (this.size() == 0) {
+            this.head = newNode;
+            return;
+        }
+
+        Node<T> current = this.head;
+        while (current.next != this.head) {
+            current = current.next;
+        }
+        current.next = newNode;
+        newNode.next = this.head;
+    }
+
+    public void formattedPrint() {
+        Node<T> current = this.head;
+        StringBuilder formattedLlist = new StringBuilder();
+
+        while (current.next.next != this.head) {
+            formattedLlist.append(current.data + " ---> ");
+        }
+        formattedLlist.append(current.data + "---> back to head");
+    }
+
 }
--- src/com/datastructures/linkedList/circular/Node.java
+++ src/com/datastructures/linkedList/circular/Node.java
@@ -1,10 +1,11 @@
 package com.datastructures.linkedlist.circular;
 
 public class Node<T> {
-    T data;
-    Node<T> next;
+    public T data;
+    public Node<T> next;
 
-    public Node(T data) {
+    public Node(T data, Node<T> next) {
         this.data = data;
+        this.next = next;
     }
 }
