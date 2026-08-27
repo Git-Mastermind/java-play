eba18f1c9b9c2c5c992a86d5053fac2babd531e2
Git-Mastermind <eshan-jha@outlook.com>
2026-08-27 12:57:17 -0700

    implement two sum in Arrays

--- src/com/datastructures/arrays/Arrays.java
+++ src/com/datastructures/arrays/Arrays.java
@@ -1,5 +1,32 @@
 package com.datastructures.arrays;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Arrays {
-    
+public class Arrays<T> {
+    private List<T> array = new ArrayList<>();
+
+    public void append(T value) {
+        array.add(value);
+    }
+    public void print() {
+        System.out.println(array);
+    }
+
+    public int size() {
+        return array.size();
+    }
+
+    public void twoSum(int sum) {
+        int index = 0;
+        StringBuilder pairsOfSums = new StringBuilder();
+
+        for (int i = 0; i < this.size(); i++) {
+            for (int j = 0; j < this.size(); j++) {
+                if (i + j == sum) {
+                    pairsOfSums.append("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        System.out.println(pairsOfSums);
+    } 
 }
--- src/com/datastructures/tests/datastructurestests/arraystests/ArrayTest.java
+++ src/com/datastructures/tests/datastructurestests/arraystests/ArrayTest.java
@@ -3,6 +3,16 @@ import com.datastructures.arrays.Arrays;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        Arrays array = new Arrays();
+        Arrays<Integer> array = new Arrays<>();
+        array.append(1);
+        array.append(2);
+        array.append(3);
+        array.append(4);
+        array.append(5);
+        array.append(6);
+        array.append(7);
+        array.append(8);
+
+        array.twoSum(5);
     }
 }
