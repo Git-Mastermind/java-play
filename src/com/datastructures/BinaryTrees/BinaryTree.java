174aad12dacfaf6db90f44ffe07f9bee14160bae
Git-Mastermind <eshan-jha@outlook.com>
2026-09-01 18:19:05 -0700

    modify binary trees and binary tree tes

--- src/com/datastructures/BinaryTrees/BinaryTree.java
+++ src/com/datastructures/BinaryTrees/BinaryTree.java
@@ -7,5 +7,21 @@ public class BinaryTree<T> {
         this.root = root;
     }
 
-    
+    public StringBuilder preOrder(StringBuilder traversalNotebook, Node<T> currentNode) {
+        if (currentNode != null) {
+            traversalNotebook.append(currentNode.value + " --> ");
+            this.preOrder(traversalNotebook, currentNode.left);
+            this.preOrder(traversalNotebook, currentNode.right);
+        }
+        return traversalNotebook;
+    }
+
+    public StringBuilder postOrder(StringBuilder traversalNotebook, Node<T> currentNode) {
+        if (currentNode != null) {
+            this.postOrder(traversalNotebook, currentNode.left);
+            this.postOrder(traversalNotebook, currentNode.right);
+            traversalNotebook.append(currentNode.value + " --> ");
+        }
+        return traversalNotebook;
+    }
 }
--- src/com/datastructures/tests/datastructurestests/binarytreetests/BinaryTreeTest.java
+++ src/com/datastructures/tests/datastructurestests/binarytreetests/BinaryTreeTest.java
@@ -7,6 +7,10 @@ public class BinaryTreeTest {
     public static void main(String[] args) {
         Node<Integer> root = new Node<>(1);
         BinaryTree<Integer> tree = new BinaryTree<>(root);
+
+        StringBuilder traversalNotebook = new StringBuilder();
+        StringBuilder treePrinted = tree.postOrder(traversalNotebook, root);
+        System.out.println(treePrinted);
     }
         
         
